19ef266cc99071d1b09564e47205f45cb56486ab
Пользователь <dm.stebenev@gmail.com>
2022-12-25 15:57:37 +0200

    less 3

---
+++ hw html_3/task_2/main.js
@@ -0,0 +1,97 @@
+// ---------block _2 case_1---------
+
+// const name= 'Vasya';
+// console.log(typeof name, ' Hello', name);
+
+
+// ---------case_2---------
+
+// const name = 123;
+// console.log(typeof name, 'Ошибка, не тот тип данных')
+
+
+// ---------case_3 ---------
+
+// console.log(typeof 1)
+// console.log(typeof 'a')
+
+
+// ---------block_2 case_1 ---------
+
+// let arr = ['Капуста', 'Репа', 'Редиска', 'Морковка'];
+// console.log(arr.join('|').toString())
+
+
+// --------case_2---------
+
+// const newArr = 'Вася;Петя;Вова;Олег';
+// console.log('newArr.log', newArr.split(';'))
+
+
+// ---------case-3---------
+
+// function hello2(name = 'Василий'){
+//     if(name) {
+//         console.log('Привет, ' + name);
+//     } else {
+//         console.log('Привет, гость');
+//     }
+// }
+// hello2();
+
+
+// ---------case-4---------
+
+// const fruits = ['яблоко', 'ананас', 'груша'];
+// const fruitsInUpperCase = fruits.toString();
+// console.log(fruitsInUpperCase.toLocaleUpperCase().split(','))
+
+
+//---------case_5---------
+
+// const val = [1, 2, 3, 4];
+// console.log(val)
+// console.log('val.log', val.map(x => x + 1))
+
+
+//---------case_6---------
+
+// const val = getsum(1, 2, 3, 4);
+// function getsum() {
+//     let result = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         result += arguments[i];
+//     }
+//     return result;
+// }
+// console.log('getsum.log', getsum(1, 2, 3, 4))
+
+
+// ---------case_7---------
+
+// const arr = [1, 'hello', 2, 3, 4, '5', '6', 7, null];
+// const numberArray = arr.filter(item => typeof item === "number");
+// console.log('numberArray.log', numberArray)
+
+
+// ---------case_8---------
+
+// const arrayTesting = [0, false, null, 1];
+// console.log(arrayTesting)
+// let element;
+// if(!!element === Boolean && element === true) {
+//         console.log('Нашли True значение');
+//     } else {
+//         console.log('Ничего нет');
+// }
+
+
+// const arrayTesting = [0, false, null, 1];
+// console.log(arrayTesting)
+// let result = !!arrayTesting === Boolean && arrayTesting === true ? 'Нашли True значение' : 'Ничего нет'
+// console.log(result)
+
+
+
+
+
